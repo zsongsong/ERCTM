9b5678f78000792b227aff516fd243a2aec4e159
zsongsong <31302754+zsongsong@users.noreply.github.com>
2022-10-13 16:42:26 +0800

    Add files via upload

---
+++ simulations/Scenario1/Detector/genDetector1.sh
@@ -0,0 +1,28 @@
+
+sumo /home/veins/workspace.omnetpp/veins/examples/veins/sc1.sumo.cfg
+cd   /home/veins/workspace.omnetpp/veins/examples/veins/Detector
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e0.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e1.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e2.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e3.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e4.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e5.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e6.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e7.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e8.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e9.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e10.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e11.xml
+rm e0.xml
+rm e1.xml
+rm e2.xml
+rm e3.xml
+rm e4.xml
+rm e5.xml
+rm e6.xml
+rm e7.xml
+rm e8.xml
+rm e9.xml
+rm e10.xml
+rm e11.xml
+
---
+++ simulations/Scenario2/Detector/genDetector2.sh
@@ -0,0 +1,63 @@
+
+sumo /home/veins/workspace.omnetpp/veins/examples/veins/sc2.sumo.cfg
+cd   /home/veins/workspace.omnetpp/veins/examples/veins/Detector
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e0.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e1.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e2.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e3.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e4.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e5.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e6.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e7.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e8.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e9.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e10.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e11.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e12.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e13.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e14.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e15.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e16.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e17.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e18.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e19.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e20.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e21.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e22.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e23.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e24.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e25.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e26.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e27.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e28.xml
+python "/home/veins/src/sumo-1.8.0/tools/xml/xml2csv.py" e29.xml
+rm e0.xml
+rm e1.xml
+rm e2.xml
+rm e3.xml
+rm e4.xml
+rm e5.xml
+rm e6.xml
+rm e7.xml
+rm e8.xml
+rm e9.xml
+rm e10.xml
+rm e11.xml
+rm e12.xml
+rm e13.xml
+rm e14.xml
+rm e15.xml
+rm e16.xml
+rm e17.xml
+rm e18.xml
+rm e19.xml
+rm e20.xml
+rm e21.xml
+rm e22.xml
+rm e23.xml
+rm e24.xml
+rm e25.xml
+rm e26.xml
+rm e27.xml
+rm e28.xml
+rm e29.xml
